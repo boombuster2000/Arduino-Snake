00c018dc099ea1e49afd3a87089d8b2fc8b54603
boombuster2000 <chanuka.williams@outlook.com>
2024-01-18 18:28:32 +0000

    Defined hardware, size, and output pins

--- Snake.ino
+++ Snake.ino
@@ -1,6 +1,14 @@
 #include "libraries/MD_MAX72XX-3.5.1/src/MD_MAX72xx.h"
 
 
+// Defining hardware, size, and output pins
+#define HARDWARE_TYPE MD_MAX72XX::moduleType_t::FC16_HW
+#define MAX_DEVICES 1 // no. of screens
+#define CS_PIN 9
+#define DATA_PIN 11
+#define CLK_PIN 13
+
+
 void setup(){
 
 }
